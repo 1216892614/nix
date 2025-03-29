f0a7a5ce2a7d45bbdbe6cad430fc755229159c73
nerd <hndppt@outlook.com>
2025-03-29 20:06:47 +0800

    fix i3

--- home-manager-cfgs/i3.nix
+++ home-manager-cfgs/i3.nix
@@ -2,5 +2,5 @@
 
 {
   home.file.".config/i3/assets/wallpaper.png".source = ../assets/wallpaper.png;
-  windowManager.i3.configFile = ./i3-config;
+  home.file.".config/i3/config".source = ./i3-config;
 }
