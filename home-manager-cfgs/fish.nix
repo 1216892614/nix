3884ebdbda801a35c4850416093cc593787f6f81
nerd <hndppt@outlook.com>
2025-03-29 18:55:44 +0800

    fix fish

--- home-manager-cfgs/fish.nix
+++ home-manager-cfgs/fish.nix
@@ -4,19 +4,19 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-    if status is-interactive
-      eval (zellij setup --generate-auto-start fish | string collect)
-    end
+if status is-interactive
+  eval (zellij setup --generate-auto-start fish | string collect)
+end
 
-    set -x EDITOR hx
-    set -x GTK_IM_MODULE "fcitx"
-    set -x QT_IM_MODULE "fcitx"
-    set -x XMODIFIERS "@im=fcitx"
+set -x EDITOR hx
+set -x GTK_IM_MODULE "fcitx"
+set -x QT_IM_MODULE "fcitx"
+set -x XMODIFIERS "@im=fcitx"
 
-    set -g fish_greeting "
-    /\_/\\
-    ( o.o )
-    > ^ <
+set -g fish_greeting "
+ /\_/\\
+( o.o )
+ > ^ <
     " 
     '';
     plugins = [
