91568e31308be2d75a13e479b6e68e852cf613f4
nerd <hndppt@outlook.com>
2025-03-29 18:44:18 +0800

    fix fish

--- home-manager-cfgs/fish.nix
+++ home-manager-cfgs/fish.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 {
-  home.file.".config/fish/config.fish".source = ./config.fish;
   programs.fish.enable = true;
+  home.file.".config/fish/config.fish".source = ./config.fish;
 }
