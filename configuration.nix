e0161f0b0a5caa21df291a8b17e425c262659b86
nerd <hndppt@outlook.com>
2025-03-29 17:19:37 +0800

    fix i3

--- configuration.nix
+++ configuration.nix
@@ -55,9 +55,6 @@
   services.xserver = {
     enable = true;
     desktopManager = {xterm.enable=false;};
-    displayManager = {
-      defaultSession = "none+i3";
-    };
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
@@ -74,12 +71,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "in";
-    xkbVariant = "eng";
-  };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
